e56d3051197722b391343fc093bcd9ed2404a078
Arthur Luiz <tutsluiz@hotmail.com>
2018-11-28 00:40:35 -0200

    Trabalho cardapio da semana android studio data 28/11/2018

---
+++ app/src/main/java/br/com/auladesenvolvimento/trabalho/MainActivity.java
@@ -0,0 +1,46 @@
+package br.com.auladesenvolvimento.trabalho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        spinner1.setOnItemSelectedListener(new ItemSelectedListener());
+    }
+
+    public class ItemSelectedListener implements AdapterView.OnItemSelectedListener{
+        //Get string of first item
+        String firstItem = String.valueOf(spinner1.getSelectedItem());
+
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (firstItem.equals(String.valueOf(spinner1.getSelectedItem()))) {
+                // ToDo when first item is selected
+            } else {
+                Toast.makeText(parent.getContext(),
+                        "You have selected : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+                // Todo when item is selected by the user
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg) {
+
+        }
+    }
+
+}
